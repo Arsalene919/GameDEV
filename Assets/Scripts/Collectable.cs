87fd5e8df14da04d869523e90574bc294d494743
Arsalene <60300140+Arsalene919@users.noreply.github.com>
2020-11-11 21:55:42 +0100

    Add empty project

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collectable : MonoBehaviour
+{
+    public GameManager GM;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            GM.Score++;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;// this will be shown in the inspector
+    [SerializeField]
+    private float _horizontalInput;
+    [SerializeField]
+    private float _verticalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(_horizontalInput, 0, 0) * _speed * Time.deltaTime);
+
+        _verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(0, _verticalInput, 0) * _speed * Time.deltaTime);
+    }
+}
